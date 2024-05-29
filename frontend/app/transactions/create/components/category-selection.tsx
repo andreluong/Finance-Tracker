0ce3f1afc8e70deff6ae1c23ebe68d954020538d
Andre Luong <97815138+andreluong@users.noreply.github.com>
2024-05-29 01:24:47 -0700

    Refactor category selection to use SWR for data fetching

--- frontend/app/transactions/create/components/category-selection.tsx
+++ frontend/app/transactions/create/components/category-selection.tsx
@@ -1,33 +1,34 @@
-import React, { ChangeEvent, FC, useEffect, useState } from 'react'
-import axios from 'axios';
+import React, { ChangeEvent, FC } from 'react'
 import { Category } from '@/app/types';
+import useSWR from 'swr';
+import { fetcher } from '@/app/lib/utils';
 
 interface CategorySelectionProps {
     onCategoryChange: (category: string) => void;
+    type : string;
 }
 
-const CategorySelection: FC<CategorySelectionProps> = ({onCategoryChange}) => {
-    const [categories, setCategories] = useState([] as Category[]);
+const CategorySelection: FC<CategorySelectionProps> = ({onCategoryChange, type}) => {
+    const url = type === 'all'
+        ? 'http://localhost:8080/api/categories'
+        : `http://localhost:8080/api/categories/${type}`;
 
-    const getCategories = async () => {
-        await axios.get('http://localhost:8080/api/categories')
-            .then(response => {
-                setCategories(response.data);
-            })
-            .catch(error => console.error(error));
-    };
-
-    useEffect(() => {
-        getCategories();
-    }, [])
-
+    const { data: categories, error, isLoading } = useSWR(url, fetcher);
+    
     const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
         onCategoryChange(event.target.value);
     }
 
     return (
-        <select name='category' id='category' className='w-full border border-gray-200 bg-white rounded p-2' onChange={handleSelectChange}>
-            {categories.map((category: Category) => (
+        <select 
+            name='category' 
+            id='category' 
+            className='w-full border border-gray-200 bg-white rounded p-2' 
+            onChange={handleSelectChange}
+        >
+            {isLoading && <option disabled>Loading...</option>}
+            {error && <option disabled>Error loading categories</option>}
+            {categories && categories.map((category: Category) => (
                 <option key={category.id} value={category.value} className='font-sans'>{category.name}</option>
             ))}
         </select>
