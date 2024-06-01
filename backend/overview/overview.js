45253263f25cce8ada9d9a1be370a0ecf01a23ed
Andre Luong <97815138+andreluong@users.noreply.github.com>
2024-05-31 19:26:17 -0700

    Fix monthly finances empty array

--- backend/overview/overview.js
+++ backend/overview/overview.js
@@ -22,11 +22,11 @@ router.get('/api/overview/monthly-frequent-spending-categories', clerkAuth, asyn
 router.get('/api/overview/monthly-finances', clerkAuth, async (req, res) => {
     const data = await database.overview.getMonthlyIncomeAndExpense(req.auth.userId);
     
-    let income = data.find((item) => item.type === 'income').total;
-    let expense = data.find((item) => item.type === 'expense').total;
+    let income = data.find((item) => item.type === 'income')?.total || 0;
+    let expense = data.find((item) => item.type === 'expense')?.total || 0;
     income = Number(income).toFixed(2);
     expense = Number(expense).toFixed(2);
-    const netIncome = income - expense;
+    const netIncome = (income - expense).toFixed(2);
 
     res.status(200).json({income, expense, netIncome});
 });
