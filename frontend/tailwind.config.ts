341ae29c5488bf1f3c0d5b73e6521e1d1620f4b7
Andre Luong <97815138+andreluong@users.noreply.github.com>
2024-06-03 16:52:29 -0700

    Initialize NextUI

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -12,9 +14,10 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      }
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()]
 };
 export default config;
