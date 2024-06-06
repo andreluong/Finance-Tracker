01f93546b2ee9b888d6f5453b05bf5e784fbf30e
Andre Luong <97815138+andreluong@users.noreply.github.com>
2024-06-06 00:06:15 -0700

    Fix all categories route name

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const categoriesController = require('../controllers/categoriesController');
 
-router.get('/api/categories', categoriesController.getAllCategories);
+router.get('/api/categories/all', categoriesController.getAllCategories);
 
 router.get('/api/categories/unique', categoriesController.getAllUniqueCategories);
 
