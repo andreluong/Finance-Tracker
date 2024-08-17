2fc6e4ee1a53e95683976366e011c33e7a953929
Andre Luong <97815138+andreluong@users.noreply.github.com>
2024-08-16 19:12:14 -0700

    💄 Enhance StatCard usability - Add tooltip to display full category name - Prevent line breaks in category display for better readability

--- frontend/app/components/ui/categories-card.tsx
+++ frontend/app/components/ui/categories-card.tsx
@@ -1,5 +1,6 @@
 import StatCard from '@/app/components/ui/stat-card'
 import { CategoryStat } from '@/app/types'
+import { Tooltip } from '@nextui-org/react'
 
 export default function CategoriesCard({
     title,
@@ -10,24 +11,39 @@ export default function CategoriesCard({
     categories: CategoryStat[],
     columns?: number
 }) {
+    const trunacteTitle = (title: string) => {
+        return title.length > 15 && columns === 3 
+            ? `${title.substring(0, 12)}...` 
+            : title
+    }
+
     return (
         <div className="bg-white border border-zinc-200 rounded-lg p-4 w-full">
             <p className="text-2xl pb-4">{title}</p>
             <div className={`grid grid-cols-${columns} gap-4`}>
                 {categories.map((category: CategoryStat, index) => (
-                    <div
-                        key={index}
-                        className="rounded-lg p-4 w-full"
-                        style={{ backgroundColor: category.colour }}
+                    <Tooltip
+                        content={category.name}
+                        placement="bottom"
+                        size='lg'
+                        shadow='sm'
                     >
-                        <StatCard
-                            item={{
-                                icon: category.icon || "carbon:unknown",
-                                title: `${category.name} (${category.count})`,
-                                amount: category.total
-                            }}
-                        />
-                    </div>
+                        <div
+                            key={index}
+                            className="rounded-lg p-4 w-full"
+                            style={{ backgroundColor: category.colour }}
+                        >
+                            
+                            <StatCard
+                                    item={{
+                                        icon: category.icon || "carbon:unknown",
+                                        title: `${trunacteTitle(category.name)} (${category.count})`,
+                                        amount: category.total
+                                    }}
+                                />
+                        </div>
+                    </Tooltip>
+
                 ))}
             </div>
         </div>
